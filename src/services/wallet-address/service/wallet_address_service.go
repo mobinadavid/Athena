6832ff7c5e96c563b518225c1d1b69a4e5ccb037
mobina <@m.davoudimoghaddam>
2024-07-27 12:29:34 +0330

    Get unallocated WalletAddress is done

--- src/services/wallet-address/service/wallet_address_service.go
+++ src/services/wallet-address/service/wallet_address_service.go
@@ -56,7 +56,7 @@ func (service *WalletAddressService) GetList() (*scopes.PaginateModel, error) {
 
 func (service *WalletAddressService) GetWalletAddress(request *request.GetWalletAddress) ([]string, error) {
 	if request.Number <= 0 {
-		return nil, errors.New("invalid number of wallet_address requested")
+		return nil, errors.New("invalid number of wallet address requested")
 	}
 
 	walletAddresses, err := service.IWalletAddressRepository.GetWalletAddress(request.Blockchain, request.Number)
